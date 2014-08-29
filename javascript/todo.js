66536d7e14c40fa0099b251b490c51b1297c50be
JordanDDisch <jordanddisch@gmail.com>
2014-08-29 22:48:55 +1000

    can add do items properly

--- javascript/todo.js
+++ javascript/todo.js
@@ -1,4 +1,6 @@
 todo = {
+    todoItems: [],
+    todoItemCount: 0,
     getTodoValue: function() {
         return document.getElementsByClassName('new-todoItem')[0].value;
     },
@@ -14,56 +16,108 @@ todo = {
         }
     },
     buildTodoList: function() {
-        var node = document.createElement("ul");
-        node.id = "todo-list";
-        document.getElementsByTagName("body")[0].appendChild(node);
         todo.todoItems.forEach(function(todoItem){
-            todo.buildTodoItem(todoItem);
+            todo.buildTodoItemLI(todoItem);
         });
-
     },
-    buildTodoItem: function(todoItem) {
+    buildTodoItemLI: function(todoItem) {
         var node = document.createElement("LI");
-        node.id = "todo-item";
-        var textnode = document.createTextNode(todoItem);
+        node.id = "todo-item-" + todoItem.id;
+        var textnode = document.createTextNode(todoItem.value);
         node.appendChild(textnode);
         var closeButton = document.createElement("div");
-        closeButton.id = "close-button";
+        closeButton.className = "close-button";
         var closeTextNode = document.createTextNode("close");
         closeButton.appendChild(closeTextNode);
         document.getElementById("todo-list").appendChild(node).appendChild(closeButton);
-        todo.saveTodoListLocally();
+    },
+    buildTodoItem: function(todoItem) {
+        todo.todoItemCount++;
+        todo.saveTodoItemLocally(todoItem);
     },
     removeTodoItem: function($) {
-        $('#close-button').click(function(){
-            $('#todo-item').remove();
+
+        $('.close-button').click(function(){
+            
+            var todoItemID = $(this).parent().attr('id');
+            todoItemID = todoItemID.replace("todo-item-", "");
+            $(this).parent().remove();
+            todoItem = {
+                "id": todoItemID
+            }
+            console.log(todoItem.id);
+            todo.removeTodoItemLocally(todoItem);
+
         });
+
+
+        
     },
-    saveTodoListLocally: function() {
-        todo.todoItems.push(todo.getTodoValue());
+    saveTodoItemLocally: function(todoItem) {
+        todoItemWithKey = {
+            "id": todo.todoItemCount,
+            "value": todoItem.value
+        };
+        todo.todoItems.push(todoItemWithKey);
         localStorage.setItem('todoList', JSON.stringify(todo.todoItems));
     },
-    buildListFromLocalStorage: function() {
-       todo.todoItems = JSON.parse(localStorage.getItem("todoList"));
-       if(todo.todoItems) {
-           todo.buildTodoList();
-       }
+    removeTodoItemLocally: function(todoItem) {
+
+        for (var k in todo.todoItems) {
+          if (!todo.todoItems.hasOwnProperty(k)) continue;
+          if (todo.todoItems[k].id === todoItem.id) {
+            delete todo.todoItems[k];
+          }
+        }
+
+        todo.todoItems.splice(todoItem, 1);
+        localStorage.setItem('todoList', JSON.stringify(todo.todoItems));
+    },
+    checkIfLocalTodoItems: function(callback, empty) {
+        todo.todoItems = JSON.parse(localStorage.getItem("todoList"));
+        if(todo.todoItems) {
+            callback();
+        }
+        else {
+            if(empty) {
+                empty();
+            } else {
+                alert("Your local storage is empty");
+            }
+        }
     },
     build: function() {
 
-        todo.buildListFromLocalStorage();
+        todo.checkIfLocalTodoItems(
+            function() {
+                todo.buildTodoList();
+            }, 
+            function() {
+                localStorage.setItem('todoList', "[]");
+            }
+        );
+
+        todo.removeTodoItem($);
+
 
         $("form").submit(function(e) {
-            e.preventDefault(); // this will prevent from submitting the form.
+            e.preventDefault(); // this will prevent submitting the form.
 
-            var todoItem = todo.getTodoValue();
+            todo.todoItems = JSON.parse(localStorage.getItem("todoList"));          
+
+            todoItem = {
+                value: todo.getTodoValue()
+            }
+
 
             todo.checkIfTodoItemIsEmpty(todoItem, function() {
 
-                todo.buildTodoList();
+                todo.buildTodoItem(todoItem);
+                todo.buildTodoItemLI(todoItem);
 
-                todo.removeTodoItem($);
-
+                
+                
+                
             });
         });
     }
