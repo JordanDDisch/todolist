8c89d10914011f14697759964ce8472acdbefb83
Rob Caracaus <rcaracaus@gmail.com>
2014-08-31 21:29:12 +1000

    4: Addedmultiple todo items to test

--- features/create-delete-todo-items.js
+++ features/create-delete-todo-items.js
@@ -1,13 +1,21 @@
-casper.test.begin('Create and delete a todo item', 2, function(test) {
+casper.test.begin('Create and delete multiple todo items', 3, function(test) {
     casper.start('http://todolist.dev', function() {
 
         casper.fill('form', {
-    	    'todo': 'asdf'
+    	    'todo': 'task 1'
 		});
 
         casper.click("input[type='submit']");
 
         test.assertExists('#todo-item-1');
+
+        casper.fill('form', {
+            'todo': 'task 2'
+        });
+
+        casper.click("input[type='submit']");
+
+        test.assertExists('#todo-item-2');
 
         this.click(".delete-task");
 
